8bcbfbe0fa4a7d27bea7f0ac371c405ab5cd80c7
花小汤 <wuchaojian2001@163.com>
2024-09-06 17:53:08 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -59,12 +59,22 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+    
+    
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route('/')
 def index():
-    user = User.query.first()  # 读取用户记录
+    # user = User.query.first()  # 读取用户记录
     movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
